960311fcbdeff30f2ecd1cd07a4a1d37449601e4
Keith Martin <keith.martin@outlook.com.au>
2012-10-25 10:21:01 +0000

    Correct issue with Installer not putting files in the correct locations.
    Changeset:96778

---
+++ DependencyAnalyzer2012/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace DependencyAnalyzer2012
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
