23caa16f2b6189c9f5ebf0ed4b0148c26d1d3c0c
Keith Martin <keith.martin@outlook.com.au>
2022-04-05 23:54:28 +1000

    Update NuGet Packages to address issues

--- DependencyAnalyzer2012/Properties/AssemblyInfoVersion.cs
+++ DependencyAnalyzer2012/Properties/AssemblyInfoVersion.cs
@@ -5,4 +5,4 @@
   
  using System.Reflection;
   
- [assembly: AssemblyFileVersion("1.0.0.49")]
+ [assembly: AssemblyFileVersion("1.0.0.56")]
--- DependencyExecutor/Properties/AssemblyInfoVersion.cs
+++ DependencyExecutor/Properties/AssemblyInfoVersion.cs
@@ -5,4 +5,4 @@
   
  using System.Reflection;
   
- [assembly: AssemblyFileVersion("1.0.0.36")]
+ [assembly: AssemblyFileVersion("1.0.0.39")]
